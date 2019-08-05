01ecf486544f8da365fe16b27effa2fd15300b11
PreetiKK <53634219+PreetiKK@users.noreply.github.com>
2019-08-05 13:39:01 +0530

    Add files via upload

---
+++ division.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<math.h>
+
+#include <bits/stdc++.h>
+using namespace std;
+string number , ans;
+int divisor;
+// A function to perform division of large numbers
+string Division(string number, int divisor)
+{
+    // As result can be very large store it in string
+    string ans;
+
+    // Find prefix of number that is larger
+    // than divisor.
+    int idx = 0;
+    int temp = number[idx] - '0';
+    while (temp < divisor)
+       temp = temp * 10 + (number[++idx] - '0');
+
+    // Repeatedly divide divisor with temp. After
+    // every division, update temp to include one
+    // more digit.
+    while (number.size() > idx)
+    {
+        // Store result in answer i.e. temp / divisor
+        ans += (temp / divisor) + '0';
+
+        // Take next digit of number
+        temp = (temp % divisor) * 10 + number[++idx] - '0';
+    }
+
+    // If divisor is greater than number
+    if (ans.length() == 0)
+        return "0";
+
+    // else return ans
+    return ans;
+}
+
+// Driver program to test longDivison()
+int main()
+{
+    cin >> number>> divisor ;
+
+    cout << Division(number, divisor)<< endl;
+    return 0;
+}
